2175bacfa98d70e3adc142160825a5e877dfa0bd
shirleywbi <swwbi@msn.com>
2021-02-13 20:30:23 -0800

    add project

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const compliment = "";
+
+  getCompliment = () => {
+    return "You look great!";
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{compliment}</Text>
+      <TouchableOpacity onPress={getCompliment}>
+        <Image source={require("./assets/heart-button.png")}></Image>
+      </TouchableOpacity>
+      <Button
+        onPress={getCompliment}
+        accessibilityLabel="Get a compliment"
+      />
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,10 @@
+# Complimentinator
+
+Hackthrob 2021
+
+A simple mobile app that gives you compliments - because who doesn't need a pick-me-up during these times.
+
+## Tech Stack
+
+React Native and Expo
+<https://reactnative.dev/docs/environment-setup>
